a2ebd17689167dbd2a38be7050464c51ff3f61c3
Blakeley Bond <burrick2003@yahoo.com>
2017-10-30 04:31:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,13 +6,13 @@ def line(customer_line)
 
 
   end
-else
-  counter = 1
-  output = "The line is currently: "
-    until counter == customer_line.size
-    output << "#{counter}. #{customer_line[counter-1]}"
-    counter += 1
-    end
+  elsif customer_line_size >= 1
+    counter = 1
+    output = "The line is currently: "
+      until counter == customer_line.size + 1
+        output << "#{counter}. #{customer_line[counter-1]}"
+        counter += 1
+      end
   puts output
 
 end

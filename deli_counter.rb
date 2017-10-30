693f1f2ab35af2cec1cbbec87fc81c0b3e729060
Blakeley Bond <burrick2003@yahoo.com>
2017-10-30 04:24:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,14 @@ def line(customer_line)
   if customer_line.size == 0
     puts "The line is currently empty."
   else
+    counter = 1
+    output = "The line is currently: "
+      until counter == customer_line.size + 1
+      output << "#{counter}. #{customer_line[counter-1]}"
+      end
+    puts "#{output}"
+
+
   end
 
 end
